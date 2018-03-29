9a14feb27420b9dfbaf2a64a68dfede312bebd08
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-03-29 18:49:02 +0900

    Implement: insertSort

--- src/insertionSort.java
+++ src/insertionSort.java
@@ -1,7 +1,7 @@
 public class insertionSort {
 
     public static void sort(int[] array){
-        for(int i = 0; i < array.length; i++){
+        for(int i = 1; i < array.length; i++){
             int j = i;
             while(j >= 1 && array[j-1] > array[j]){
                 int tmp = array[j];
