306c6fe4d3bc14b85b7127f4d6a71cc046e148fa
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-03-29 18:47:27 +0900

    Implement: insertSort

---
+++ src/insertionSort.java
@@ -0,0 +1,30 @@
+public class insertionSort {
+
+    public static void sort(int[] array){
+        for(int i = 0; i < array.length; i++){
+            int j = i;
+            while(j >= 1 && array[j-1] > array[j]){
+                int tmp = array[j];
+                array[j] = array[j - 1];
+                array[j-1] = tmp;
+                j --;
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+        int[] test
+                = { 10, 75, 24, 32, 98,
+                72, 88, 43, 60, 35,
+                54, 62,  2, 12, 82,
+        };
+        insertionSort.sort(test);
+        //ソートの結果の確認
+        for( int i=0; i<test.length; i++ ) {
+            System.out.println( (i+1) + ":" + test[i] );
+        }
+
+    }
+}
